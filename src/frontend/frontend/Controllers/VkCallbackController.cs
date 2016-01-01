10cc7c41d02e403d26b67273301e28034ca3bb16
quckly <quckly.max@gmail.com>
2016-01-02 01:42:14 +0500

    Started mysql connection.

--- src/frontend/frontend/Controllers/VkCallbackController.cs
+++ src/frontend/frontend/Controllers/VkCallbackController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+
+using Newtonsoft.Json.Linq;
 
 namespace VKMatcher.Frontend.Controllers
 {
@@ -11,6 +14,7 @@ namespace VKMatcher.Frontend.Controllers
     {
         public async Task HandleAsync(HttpListenerRequest request, HttpListenerResponse responce)
         {
+            string accessToken = null;
             var code = request.QueryString.Get("code");
 
             if (code == null)
@@ -19,7 +23,29 @@ namespace VKMatcher.Frontend.Controllers
                 return;
             }
 
-            
+            using (HttpClient client = new HttpClient())
+            {
+                string requestUrl = @"https://oauth.vk.com/access_token?client_id=5210253" +
+                                    @"&client_secret=Kh4v7BmzWig4ROhNmQ8S" + // TODO: transfer to private config
+                                    @"&redirect_uri=https://vk.quckly.ru/vk/callback" +
+                                    @"&code=" + code;
+
+                var vkHttpResponce = await client.GetAsync(requestUrl);
+                var vkResponceString = await vkHttpResponce.Content.ReadAsStringAsync();
+
+                dynamic vkResponce = JObject.Parse(vkResponceString);
+
+                if (vkResponce.access_token == null)
+                {
+                    await responce.ResponseErrorAsync(403);
+                    return;
+                }
+
+                accessToken = vkResponce.access_token;
+            }
+
+            // Access token successfully taken.
+
         }
     }
 }
---
+++ src/frontend/frontend/DBContext/DbConnection.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace VKMatcher.Frontend.DBContext
+{
+    class DbConnection : IDisposable
+    {
+        MySqlConnection connection;
+
+        public DbConnection()
+        {
+            var mysqlCSB = new MySqlConnectionStringBuilder();
+            mysqlCSB.Server = "";
+            mysqlCSB.Database = "";
+            mysqlCSB.UserID = "";
+            mysqlCSB.Password = "";
+
+            connection = new MySqlConnection(mysqlCSB.ConnectionString);
+            connection.Open();
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // Для определения избыточных вызовов
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: освободить управляемое состояние (управляемые объекты).
+                    connection.Dispose();
+                }
+
+                // TODO: освободить неуправляемые ресурсы (неуправляемые объекты) и переопределить ниже метод завершения.
+                // TODO: задать большим полям значение NULL.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: переопределить метод завершения, только если Dispose(bool disposing) выше включает код для освобождения неуправляемых ресурсов.
+        // ~DbConnection() {
+        //   // Не изменяйте этот код. Разместите код очистки выше, в методе Dispose(bool disposing).
+        //   Dispose(false);
+        // }
+
+        // Этот код добавлен для правильной реализации шаблона высвобождаемого класса.
+        public void Dispose()
+        {
+            // Не изменяйте этот код. Разместите код очистки выше, в методе Dispose(bool disposing).
+            Dispose(true);
+            // TODO: раскомментировать следующую строку, если метод завершения переопределен выше.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
