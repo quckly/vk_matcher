c2ff5b766f31842bcae38ac4bf59e97fc70c90ed
quckly <quckly.max@gmail.com>
2016-01-08 00:01:19 +0500

    Fetch groups and sort users.

--- src/vk_matcher/vk_matcher_core/Matching.cs
+++ src/vk_matcher/vk_matcher_core/Matching.cs
@@ -1,14 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-
 
 using Newtonsoft.Json;
 using VkNet;
 using VkNet.Model;
 using VkNet.Enums.Filters;
+
+namespace Vk.Models
+{
+    public class User
+    {
+        // General fields
+        public long id;
+        public string first_name;
+        public string last_name;
+        public string deactivated;
+        public int? hidden;
+
+        // Addons
+        public string photo_100;
+    }
+}
 
 namespace VKMatcher.Core
 {
@@ -39,21 +56,60 @@ namespace VKMatcher.Core
     public class VkApiGetFriendExecuteFriendsList
     {
         public long count;
-        public long[] items; // May be array of objects
+        public Vk.Models.User[] items; // May be array of objects
     }
 
     public class VkApiGetFriendExecuteUser
     {
         public long userId;
         public VkApiGetFriendExecuteFriendsList friends;
+        //public long[] friends;
+    }
+
+    public class VkApiExecuteResponse<TResponse>
+    {
+        public TResponse response;
+    }
+
+    class MatchedGroup
+    {
+        public long Id { get; }
+        public double Weight { get; }
+
+        public MatchedGroup(long id, double weight)
+        {
+            Id = id;
+            Weight = weight;
+        }
+    }
+
+    class MatchedUser : IComparable<MatchedUser>
+    {
+        public VkUser User { get; }
+        public double Likely { get; }
+        public List<long> MatchedGroups { get; }
+
+        public MatchedUser(VkUser user, double likely, List<long> matchedGroups)
+        {
+            User = user;
+            Likely = likely;
+            MatchedGroups = matchedGroups;
+        }
+
+        public int CompareTo(MatchedUser other)
+        {
+            return other.Likely.CompareTo(Likely);
+        }
     }
 
     public class Matching
     {
+        public readonly int MaxConsiderGroups = 30;
+
         VkApi vkApi;
         VkUser selfUser;
 
-        Dictionary<long, VkUser> considerUsers;
+        Dictionary<long, VkUser> considerUsers = new Dictionary<long, VkUser>();
 
         public Matching(string accessToken, uint userId)
         {
@@ -65,6 +121,12 @@ namespace VKMatcher.Core
 
         void AddUniqueUserToConsider(VkUser user)
         {
+            //// !!!
+            // REMOVE ME:!!!
+            //if (considerUsers.Count >= 200)
+            //    return;
+            ///!!!
+            /// !!!
             if (!considerUsers.ContainsKey(user.User.Id))
             {
                 considerUsers.Add(user.User.Id, user);
@@ -78,11 +140,11 @@ namespace VKMatcher.Core
 
             var i = 0;
             while (i < users.length)
-            {
-                result.push({ userId: users[i], groups: API.groups.get({ user_id: users[i], count: {1} })});
+            {{
+                result.push({{ userId: users[i], groups: API.groups.get({{ user_id: users[i], count: {1} }})}});
 
                 i = i + 1;
-            }
+            }}
 
             return result;", String.Join(",", userIds.Select(x => x.ToString())), count);
         }
@@ -94,36 +156,78 @@ namespace VKMatcher.Core
 
             var i = 0;
             while (i < users.length)
-            {
-                result.push({ userId: users[i], friends: API.friends.get({ user_id: users[i] })});
+            {{
+                result.push({{ userId: users[i], friends: API.friends.get({{ user_id: users[i], fields: ""deactivated"" }})}});
 
                 i = i + 1;
-            }
+            }}
 
             return result;", String.Join(",", userIds.Select(x => x.ToString())));
         }
 
+        public string ExecuteVkApiMethod(string methodName, IDictionary<string, string> parameters, string accessToken = null)
+        {
+            string url = $"https://api.vk.com/method/{methodName}";
+
+            parameters.Add("v", "5.42");
+
+            if (accessToken != null && !parameters.ContainsKey("access_token"))
+            {
+                parameters.Add("access_token", accessToken);
+            }
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = null;
+
+                int countOfError = 10;
+                while (response == null && countOfError-- > 0)
+                {
+                    try
+                    {
+                        var responseTask = client.PostAsync(url, new FormUrlEncodedContent(parameters));
+                        responseTask.Wait();
+                        response = responseTask.Result;
+                    }
+                    catch { }
+                }
+
+                return response.Content.ReadAsStringAsync().Result;
+            }
+        }
+
         public void FillConsiderUsersGroups(List<VkUser> users)
         {
-            for (int i = 0; i < users.Count; i += 25)
+            for (int i = 0; i < users.Count && i <= 5000; i += 25) // REMOVE ME
             {
-                string executeCode = BuildGetGroupsRequest(users.Skip(i).Take(25).Select(user => user.User.Id));
+                string executeCode = BuildGetGroupsRequest(users.Skip(i).Take(25).Select(user => user.User.Id), MaxConsiderGroups);
 
-                string vkApiResponse = vkApi.Invoke("execute", new Dictionary<string, string>() { { "code", executeCode } }, true);
-                var response = JsonConvert.DeserializeObject<VkApiGetGroupsExecuteUser[]>(vkApiResponse);
+                string vkApiResponse = ExecuteVkApiMethod("execute", new Dictionary<string, string>() { { "code", executeCode } }, vkApi.AccessToken);
+                var response = JsonConvert.DeserializeObject<VkApiExecuteResponse<VkApiGetGroupsExecuteUser[]>>(vkApiResponse).response;
 
                 foreach (var group in response)
                 {
                     considerUsers[group.userId].Groups = group.groups.items.ToList();
                 }
+
+                // REMOVE ME
+                Console.WriteLine($"Done {i * 100.0 / (double)users.Count}% ({i}/{users.Count})");
             }
+        }
+
+        public double GroupWeightMatchFactor(double linearGroupWeight)
+        {
+            return Math.Exp(linearGroupWeight * 2.4);
         }
 
         public string GetMatchingJsonResponse()
         {
             // First, get friends;
-            selfUser.Friends = vkApi.Friends.Get(selfUser.User.Id).Select(friend => new VkUser(friend)).ToList();
-
+            selfUser.Friends = vkApi.Friends.Get(selfUser.User.Id, ProfileFields.FirstName|ProfileFields.Photo100)
+                .Where(friend => friend.Deactivated == null)
+                .Select(friend => new VkUser(friend))
+                .ToList();
+            
             Dictionary<long, VkUser> friendsDict = new Dictionary<long, VkUser>();
             selfUser.Friends.ForEach(x => friendsDict.Add(x.User.Id, x));
             
@@ -132,12 +236,14 @@ namespace VKMatcher.Core
             {
                 string executeCode = BuildGetFriendsRequest(selfUser.Friends.Skip(i).Take(25).Select(user => user.User.Id));
 
-                string vkApiResponse = vkApi.Invoke("execute", new Dictionary<string, string>() { { "code", executeCode } }, true);
-                var response = JsonConvert.DeserializeObject<VkApiGetFriendExecuteUser[]>(vkApiResponse);
+                string vkApiResponse = ExecuteVkApiMethod("execute", new Dictionary<string, string>() { { "code", executeCode } }, vkApi.AccessToken);
+                var response = JsonConvert.DeserializeObject<VkApiExecuteResponse<VkApiGetFriendExecuteUser[]>>(vkApiResponse).response;
                 
                 foreach (var friend in response)
                 {
-                    friendsDict[friend.userId].Friends = friend.friends.items.Select(frId => new VkUser(new User() { Id = frId })).ToList();
+                    friendsDict[friend.userId].Friends = friend.friends.items
+                        .Where(u => u.deactivated == null)
+                        .Select(u => new VkUser(new User() { Id = u.id, Photo100 = new Uri(u.photo_100) })).ToList();
                 }
             }
 
@@ -162,13 +268,74 @@ namespace VKMatcher.Core
             var considerUsersList = considerUsers.Values.ToList();
 
             // Fetch all groups
-            selfUser.Groups = vkApi.Groups.Get(selfUser.User.Id, false, null, null, 0, 30).Select(x => x.Id).ToList();
+            selfUser.Groups = vkApi.Groups.Get(selfUser.User.Id, false, null, null, 0, (uint)MaxConsiderGroups).Select(x => x.Id).ToList();
+
+            // Very long operation:
             FillConsiderUsersGroups(considerUsersList);
 
             // And sort all consider users.
 
 
-            return "";
+            // Group weight : linear function declared in [0,1]
+            // = 1 - groupIndex / MaxConsiderGroups;
+
+            // Group match cost: http://www.wolframalpha.com/input/?i=plot+%28%28e^%28%28x%29+*+2.4%29%29^%281.5%29+%29+from+0+to+1
+            // F: (e^((x) * 2.4)), x in [0,1]
+
+            Dictionary<long, MatchedGroup> selfGroupsIdx = new Dictionary<long, MatchedGroup>();
+
+            //System.Diagnostics.Debug.Assert(selfUser.Groups.Count <= MaxConsiderGroups);
+
+            for (int i = 0; i < selfUser.Groups.Count && i < MaxConsiderGroups; i++)
+            {
+                long id = selfUser.Groups[i];
+                double weight = GroupWeightMatchFactor(1.0 - ((double)i / (double)MaxConsiderGroups));
+
+                selfGroupsIdx.Add(id, new MatchedGroup(id, weight));
+            }
+
+            // Compare all users to self.
+            List<MatchedUser> matchedUsers = new List<MatchedUser>(considerUsersList.Count);
+
+            foreach (var matchingUser in considerUsersList)
+            {
+                var groups = matchingUser.Groups;
+
+                if (groups == null)
+                {
+                    continue;
+                }
+
+                //System.Diagnostics.Debug.Assert(groups.Count <= MaxConsiderGroups);
+
+                List<long> matchedByUserGroups = new List<long>();
+                double sumOfLikely = 0.0;
+
+                // Collect groups match likes
+                for (int i = 0; i < groups.Count && i < MaxConsiderGroups; i++)
+                {
+                    // If matchingUser group intersect self user group
+                    if (selfGroupsIdx.ContainsKey(groups[i]))
+                    {
+                        double groupWeight = GroupWeightMatchFactor(1.0 - ((double)i / (double)MaxConsiderGroups));
+
+                        double groupLikely = groupWeight * selfGroupsIdx[groups[i]].Weight;
+
+                        sumOfLikely += groupLikely;
+                        matchedByUserGroups.Add(groups[i]);
+                    }
+                }
+
+                // Add current user with likely weight to result list
+                matchedUsers.Add(new MatchedUser(matchingUser, sumOfLikely, matchedByUserGroups));
+            }
+
+            // Finally sort users, get top, and return result.
+            matchedUsers.Sort();
+
+            var resultUsers = matchedUsers.Take(20).Select(u => new { Likely = u.Likely, UserId = u.User.User.Id, Groups = u.MatchedGroups, Photo = u.User.User.Photo100 });
+
+            return JsonConvert.SerializeObject(resultUsers);
         }
     }
 }
--- src/vk_matcher/vk_matcher_core/VkMatcherServer.cs
+++ src/vk_matcher/vk_matcher_core/VkMatcherServer.cs
@@ -34,7 +34,8 @@ namespace VKMatcher.Core
                 var matcher = new Matching(accessToken, userId);
                 return matcher.GetMatchingJsonResponse();
             }
-            catch {
+            catch (Exception e) {
+                logger.Log(e.Message);
                 return null;
             }
         }
@@ -56,79 +57,86 @@ namespace VKMatcher.Core
 
         void DoWork()
         {
-            MySqlCommand queryGetFreeTask = null, querySetResponse = null;
-
-            try
+            while (true)
             {
-                Random rand = new Random();
-                queryGetFreeTask = getQueryGetFreeTask();
-                queryGetFreeTask.Connection.Open();
-                querySetResponse = getQuerySetResponse();
-                querySetResponse.Connection.Open();
+                MySqlCommand queryGetFreeTask = null, querySetResponse = null;
 
-                int idleTime = 0;
-
-                while (true)
+                try
                 {
-                    if (queryGetFreeTask.Connection.State != System.Data.ConnectionState.Open)
-                    {
-                        queryGetFreeTask = getQueryGetFreeTask();
-                        queryGetFreeTask.Connection.Open();
-                    }
+                    Random rand = new Random();
+                    queryGetFreeTask = getQueryGetFreeTask();
+                    queryGetFreeTask.Connection.Open();
+                    querySetResponse = getQuerySetResponse();
+                    querySetResponse.Connection.Open();
 
-                    using (var reader = queryGetFreeTask.ExecuteReader())
+                    int idleTime = 0;
+
+                    while (true)
                     {
-                        if (reader.Read()) // if - because only one line in result, otherwise must use while
+                        if (queryGetFreeTask.Connection.State != System.Data.ConnectionState.Open)
                         {
-                            int id = reader.GetInt32("id");
-                            uint user_id = reader.GetUInt32("user_id");
-                            string access_token = reader.GetString("access_token");
-
-                            string response = CalcVkApiMatching(access_token, user_id);
-
-                            if (response == null)
-                            {
-                                // log it, set record error state
-                                continue;
-                            }
-
-                            if (querySetResponse.Connection.State != System.Data.ConnectionState.Open)
-                            {
-                                querySetResponse = getQuerySetResponse();
-                                querySetResponse.Connection.Open();
-                            }
-
-                            querySetResponse.Parameters["@id"].Value = id;
-                            querySetResponse.Parameters["@response"].Value = response;
-                            querySetResponse.ExecuteNonQuery();
-
-                            idleTime = 0;
+                            queryGetFreeTask = getQueryGetFreeTask();
+                            queryGetFreeTask.Connection.Open();
                         }
-                        else
+
+                        using (var reader = queryGetFreeTask.ExecuteReader())
                         {
-                            // If no result in select don't spam a DB for nothing.
-                            idleTime = Math.Min(idleTime + 90, 2000);
-                            Thread.Sleep(idleTime + rand.Next(20));
+                            if (reader.Read()) // if - because only one line in result, otherwise must use while
+                            {
+                                int id = reader.GetInt32("id");
+                                uint user_id = reader.GetUInt32("user_id");
+                                string access_token = reader.GetString("access_token");
+
+                                string response = CalcVkApiMatching(access_token, user_id);
+
+                                if (response == null)
+                                {
+                                    // log it, set record error state
+                                    continue;
+                                }
+
+                                if (querySetResponse.Connection.State != System.Data.ConnectionState.Open)
+                                {
+                                    querySetResponse = getQuerySetResponse();
+                                    querySetResponse.Connection.Open();
+                                }
+
+                                querySetResponse.Parameters["@id"].Value = id;
+                                querySetResponse.Parameters["@response"].Value = response;
+                                querySetResponse.ExecuteNonQuery();
+
+                                idleTime = 0;
+                            }
+                            else
+                            {
+                                // If no result in select don't spam a DB for nothing.
+                                idleTime = Math.Min(idleTime + 90, 2000);
+                                Thread.Sleep(idleTime + rand.Next(20));
+                            }
                         }
                     }
                 }
-            }
-            catch (Exception e)
-            {
-                logger.Log(e.Message);
-            }
-            finally
-            {
-                if (queryGetFreeTask != null)
+                catch (MySqlException e)
                 {
-                    queryGetFreeTask.Dispose();
-                    queryGetFreeTask.Connection.Dispose();
+                    logger.Log($"Help: {e.HelpLink}, Message: {e.Message}, Source: {e.Source}, Number: {e.Number}");
                 }
-
-                if (querySetResponse != null)
+                catch (Exception e)
                 {
-                    querySetResponse.Dispose();
-                    querySetResponse.Connection.Dispose();
+                    logger.Log(e.Message);
+                }
+                finally
+                {
+                    if (queryGetFreeTask != null)
+                    {
+                        queryGetFreeTask.Dispose();
+                        queryGetFreeTask.Connection.Dispose();
+                    }
+
+                    if (querySetResponse != null)
+                    {
+                        querySetResponse.Dispose();
+                        querySetResponse.Connection.Dispose();
+                    }
                 }
             }
         }
